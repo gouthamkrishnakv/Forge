ca4c10fcfb818d15a9a15ffd000b0deb390f35db
Goutham Krishna K V <gauthamkrishna9991@live.com>
2022-12-27 12:59:43 +0530

    refactor: Reformat code to opinionated black style

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
 import sys
 import gi
-import json
 
-gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
-
-from .main_window import MainWindow
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
 
 from gi.repository import Gtk, Gio, Adw
 
-class TempestApplication(Adw.Application):
+from .main_window import MainWindow
+
+
+class ForgeApplication(Adw.Application):
     def __init__(self) -> None:
         super().__init__(
-            application_id="com.crosine.Forge",
-            flags=Gio.ApplicationFlags.FLAGS_NONE
+            application_id="com.crosine.Forge", flags=Gio.ApplicationFlags.FLAGS_NONE
         )
+        self.add_default_actions()
+
+    def add_default_actions(self):
+        self.create_action("quit", self._quit, ["<primary>q"])
+
+    def _quit(self, *__args):
+        self.quit()
 
     def do_activate(self):
         win: Gtk.Window = self.props.active_window
@@ -22,8 +28,15 @@ class TempestApplication(Adw.Application):
             win = MainWindow(application=self)
         win.present()
 
+    def create_action(self: str, name, callback, shortcuts=None):
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)
+
 
 def main(version):
     """Application entry point."""
-    app = TempestApplication()
+    app = ForgeApplication()
     return app.run(sys.argv)
--- src/main_window.py
+++ src/main_window.py
@@ -1,15 +1,17 @@
 import gi
 
-gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
 
 from gi.repository import Gtk, Adw
+
 
 @Gtk.Template(resource_path="/com/crosine/Forge/UI/main_window.ui")
 class MainWindow(Adw.ApplicationWindow):
     __gtype_name__ = "MainWindow"
-    
+
     label = Gtk.Template.Child()
-    
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.label.set_label("Hello, Goutham!")
